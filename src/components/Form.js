153f92ac437f177564e19d8c6785fa50b38de8e9
Alejo Boga <alejoboga20@gmail.com>
2021-07-12 09:49:47 -0300

    Creating form content

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,38 @@ import React from 'react';
 
 export const Form = () => {
   return (
-    <div>
-      <h1>Form</h1>
-    </div>
+    <>
+      <h2>Create Appoinment</h2>
+
+      <form>
+        <label>Pet's Name</label>
+        <input
+          type='text'
+          name='pet'
+          className='u-full-width'
+          placeholder="Pet's Name"
+        />
+
+        <label>Owner's Name</label>
+        <input
+          type='text'
+          name='owner'
+          className='u-full-width'
+          placeholder="Owner's Name"
+        />
+
+        <label>Date</label>
+        <input type='date' name='date' className='u-full-width' />
+
+        <label>Time</label>
+        <input type='time' name='time' className='u-full-width' />
+
+        <label>Symptoms</label>
+        <textarea className='u-full-width' name='symptoms'></textarea>
+        <button type='submit' className='u-full-width button-primary'>
+          Add Appoinment
+        </button>
+      </form>
+    </>
   );
 };
