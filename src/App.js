162c7b510295fd37af5cf0498584bf1b88099d76
Alejo Boga <alejoboga20@gmail.com>
2021-07-12 09:43:12 -0300

    Creating form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Form } from './components/Form';
 
 export const App = () => {
   return (
     <div>
       <h1>Patients Manager</h1>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='one-half column'>
+            <Form />
+          </div>
+          <div className='one-half column'>2</div>
+        </div>
+      </div>
     </div>
   );
 };
